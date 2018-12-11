58a4eb970253341e0b36cdf47d434db242bf246f
dabauer <colinpseud@gmail.com>
2018-12-11 16:07:21 +0100

    MAJ xml et main sql pour les rencontres

--- mainsql.sql
+++ mainsql.sql
@@ -7,51 +7,52 @@ DROP TABLE IF EXISTS Rencontres;
 DROP TABLE IF EXISTS Sport;
 
 CREATE TABLE Joueurs(
-  id SERIAL PRIMARY KEY,
-  nom VARCHAR(50) NOT NULL,
-  prenom VARCHAR(50) NOT NULL,
-  age INTEGER NOT NULL
+  id serial PRIMARY KEY,
+  nom varchar(50) NOT NULL,
+  prenom varchar(50) NOT NULL,
+  age integer NOT NULL
 );
 
-CREATE TABLE Rencontres (
-  id SERIAL PRIMARY KEY,
-  date DATE NOT NULL
-);
 
 CREATE TABLE Equipe (
-  id SERIAL PRIMARY KEY,
-  pays VARCHAR(30) NOT NULL
+  id serial PRIMARY KEY,
+  pays varchar(30) NOT NULL
 );
 
 
 CREATE TABLE EquipesJoueurs (
-  id SERIAL PRIMARY KEY,
-  joueur INTEGER NOT NULL REFERENCES Joueurs (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  equipe INTEGER NOT NULL REFERENCES Equipe (id) ON DELETE CASCADE ON UPDATE CASCADE
+  id serial PRIMARY KEY,
+  joueur integer NOT NULL REFERENCES Joueurs (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  equipe integer NOT NULL REFERENCES Equipe (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE Sport (
-  id SERIAL PRIMARY KEY,
-  nom VARCHAR(50)
+  id serial PRIMARY KEY,
+  nom varchar(50)
 );
 
 
 CREATE TABLE Epreuve (
-  id SERIAL PRIMARY KEY,
-  sexe CHAR(1),
-  nom TEXT NOT NULL,
-  lieu TEXT NOT NULL,
-  sport INTEGER NOT NULL REFERENCES Sport (id) ON DELETE CASCADE ON UPDATE CASCADE
+  id serial PRIMARY KEY,
+  sexe char(1),
+  nom text NOT NULL,
+  lieu text NOT NULL,
+  sport integer NOT NULL REFERENCES Sport (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
+CREATE TABLE Rencontres (
+  id serial PRIMARY KEY,
+  date date
+)
+
 CREATE TABLE Resultat (
-  id SERIAL PRIMARY KEY,
-  Equipe INTEGER REFERENCES Equipe (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  Epreuve INTEGER REFERENCES Epreuve (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  Rencontres INTEGER REFERENCES Rencontres (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  Position INTEGER,
-  Temps TIME,
-  Points INTEGER
+  id serial PRIMARY KEY,
+  equipe integer REFERENCES Equipe (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  epreuve integer REFERENCES Epreuve (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  rencontres integer REFERENCES Rencontres (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  position integer,
+  temps time,
+  points integer
 );
