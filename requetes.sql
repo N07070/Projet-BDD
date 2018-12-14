e109e483c6f106b8e7bf82d20a79457ca4c82286
N07070 <->
2018-12-14 15:48:26 +0100

    Mise en page Ecriture des premieres requetes

--- requetes.sql
+++ requetes.sql
@@ -1,14 +1,73 @@
 -- Requetes
 
 -- Exo 1
+\echo "DIFFICULTE ٭"
+-- 1)
+\echo "1) ------------"
+-- Selectionne les joueurs italiens ayant eu une médaille
+SELECT * FROM joueurs WHERE ( SELECT * FROM EquipesJoueurs WHERE
+	-- Selection toutes les équipes qui ont eu une médaille
+	(SELECT Equipes FROM resultats where position = 1 OR position = 2 OR position = 3
+		-- Selectionne les équipes italiennes
+		AND WHERE Equipes.pays = 'ITA'))
+-- 2)
+\echo "2) ------------"
+CREATE VIEW v(Nom, Pays) AS SELECT Nom, Equipe.Pays FROM EquipesJoueurs WHERE Resultats.Equipes = EquipeJoueurs.id AND SELECT Pays FROM Equipes WHERE
+	-- Selectionne tout les resultats du 100, 200 et 400m où y'a eu une médaille
+	( SELECT * FROM resultats WHERE position = 1 OR position = 2 OR position = 3 AND Resultats.Epreuves.nom = "100 metres" OR Resultats.Epreuves.nom = "200 metres" OR Resultats.Epreuves.nom = "400 metres" )
 
+-- 3)
+\echo "3) ------------"
+
+-- 4)
+\echo "4) ------------"
+
+-- 5)
+\echo "5) ------------"
+
+-- 6)
+\echo "6) ------------"
 
 -- Exo 2
+\echo "DIFFICULTE ٭٭"
+-- 1)
+\echo "1) ------------"
 
+-- 2)
+\echo "2) ------------"
+
+-- 3)
+\echo "3) ------------"
+
+-- 4)
+\echo "4) ------------"
+
+-- 5)
+\echo "5) ------------"
+
+-- 6)
+\echo "6) ------------"
 
 -- Exo 3
+\echo "DIFFICULTE ٭٭٭"
 
---- Testes
+-- 1)
+\echo "1) ------------"
 
--- Récupère toutes les joueurs ayant eu des médailles
-select * from joueurs where (select joueur from natural join EquipesJoueurs , Equipes on EquipesJoueurs.Equipes = Equipe.id where ( select * from resultats where position is 1 OR position is 2 OR position is 3))
+-- 2)
+\echo "2) ------------"
+
+-- 3)
+\echo "3) ------------"
+
+-- 4)
+\echo "4) ------------"
+
+-- 5)
+\echo "5) ------------"
+
+-- 6)
+\echo "6) ------------"
+
+--- Tests
+\echo "TESTING - TESTING - TESTING - TESTING"
