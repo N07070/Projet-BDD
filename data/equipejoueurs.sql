ac45976b26470d5a971b5aadd52cfb7be1dc19cf
N07070 <->
2018-12-14 13:28:10 +0100

    Ajout des pays ( equipes )

---
+++ data/equipejoueurs.sql
@@ -0,0 +1,5 @@
+-- les equipes sont faites en fonction de leurs pays
+
+INSERT INTO equipesjoueurs (joueurs, equipe) VALUES
+
+(1,),
--- data/equipes.sql
+++ data/equipes.sql
@@ -1,1 +1,18 @@
-
+INSERT INTO equipe( pays ) VALUES
+('FRA'), -- France
+('GER'), -- Allemange
+('SUI'), -- Suisse
+('ITA'), -- Italie
+('NGR'), -- Nigeria
+('JPN'), -- Japon
+('GBR'), -- UK
+('USA'),
+('BRA'), -- Brésil
+('POL'), -- Pologne
+('NED'), -- Pays bas
+('UKR'), -- Ukraine
+('KOR'), -- Corée du Sud
+('RSA'), -- Afrique du sud
+('GRE'), -- Grèce
+('ANG'), -- Angola
+('PRK'); -- DPRK / Corée du Nord
