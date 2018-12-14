b0cdf47640fc5a58012c88ce6ad34b3887fc3638
N07070 <->
2018-12-14 14:57:36 +0100

    Ajout des équipes

--- data/equipes.sql
+++ data/equipes.sql
@@ -1,19 +1,85 @@
+INSERT INTO equipe( pays ) VALUES
+
 -- equipes d'athétisme fem
 -- 100m
+('ITA'),
+('NGR'),
+('JPN'),
 -- 200m
+('BRA'),
+('ITA'),
+('SUI'),
 -- 400m
+('SUI'),
+('USA'),
+('ITA'),
 
 -- equipe handball fem
+('JPN'),
+('USA'),
+('FRA'),
+('KOR'),
 
 -- equipe de natation
 -- relais 4*100
+('USA'),
+('POL'),
+('PRK'),
 -- 200 m pap
+('PRK'),
+('RSA'),
+('ANG')
 -- relais 4*200
+('PRK'),
+('FRA'),
+('GBR'),
 -- 200m 4 nages
+('NED'),
+('SUI'),
+('GBR'),
 -- 100 m pap
+('NGR'),
+('GRE'),
+('JPN'),
 -- relais 4*100m 4 nages
+('UKR'),
+('KOR'),
+('RSA'),
 -- 200m f
+('USA'),
+('GER'),
+('FRA'),
 -- 200m m
+('POL'),
+('NED'),
+('ANG'),
+
+
+-- Football
+('JPN'),
+('FRA'),
+('GER'),
+
+-- Badminton
+('JPN'),
+('KOR'),
+('RSA'),
+-- K1 200M
+('JPN'),
+('PRK'),
+('RSA')
+-- BMX
+('JPN'),
+('SUI'),
+('GRE')
+-- Lutte 70kg
+('JPN'),
+('GRE'),
+('ITA')
+-- Beach volley equipe de 2
+('JPN'),
+('USA'),
+('NED'),
 
 
 INSERT INTO equipe( pays ) VALUES
