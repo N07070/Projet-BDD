8df971221d98c0b8a14ef0cd165f2b5bd35e6dea
N07070 <nelson.perdriau@gmail.com>
2018-12-15 19:35:33 +0100

    Ajout des lieux

---
+++ data/lieu.sql
@@ -0,0 +1,18 @@
+INSERT INTO Lieu(nom) VALUES
+("Carioca 2"), -- Jugo et Lutte
+("Carioca 3"), -- Epee
+("Olympic Aquatic Stadium"), -- Natation et Waterpolo
+("Olympic Tennis Center"),
+("Deodoro Olympic Whitewater Stadium"), -- Canoe
+("Youth Arena"), -- Basketball
+("Maracana Stadium"), -- Foot et Athletisme
+("Maria Lenk"), -- Plongeon
+("Olympic Equestrian Center"), -- Poney
+("Olympic Shooting Center"),
+("Riocentro P4"), -- Badminton
+("Copacabana Stadium"), -- Volleyball / BeachVolley
+("Deodoro Stadium"), -- Rugby
+("Future Arena"), -- Handball
+("Mountain Bike Center"), -- VTT
+("Riocentro P2"), -- Halterophilie
+("Sambodromo") -- Tir à l'arc
