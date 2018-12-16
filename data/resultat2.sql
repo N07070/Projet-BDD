a2cc48c8a271240f5c207ea0a55b08486b5b179f
dabauer <colinpseud@gmail.com>
2018-12-16 11:48:20 +0100

    maj

---
+++ data/resultat2.sql
@@ -0,0 +1,106 @@
+--e pour epreuve r pour rencontre
+INSERT INTO resultat(epreuve,rencontres,position,temps,points) VALUES
+ 
+--Athletisme
+--F
+--100m e 4 r 1
+(4,1,1,'00:00:09.789',null),
+(4,1,2,'00:00:09.885',null),
+(4,1,3,'00:00:10.002',null),
+--200m e 5 r 2
+--400m e 6 r 3
+--M
+--100m e 1 r 4
+--200m e 2 r 5
+--400m e 3 r 6
+--Marathon e 8 r 7
+--110m haies e 7 r 8
+--3000m obstacles e 9 r 9
+--triple saut e 10 r 10
+
+--hand M e 12 
+--T1 ANGvPRK r 11
+--T2 ITAvPRK r 12
+--T3 ANGvITA r 13
+
+--hand F e 11
+--T1 JPNvUSA r 14
+--T2 FRAvFRA r 15
+--T3 FRAvUSA r 16
+
+--Natation
+
+--4x100nagelibre e 13 r 17
+--4x200nl e 15 r 18
+--4x1004n e 18 r 19
+
+--200mpap e 14 r 20
+--200m4n e 16 r 21
+--100mpap e 17 r 22
+
+--200m nl H e 20
+--1/2 r 23
+--F r 24
+
+--200m nl F e 19
+--1/2 r 25
+--F r 26
+
+--Football f e 21
+--T1 ITAvGBR r 27
+--T2 GERvGBR r 28
+--T3 ITAvGER r 29
+
+-- Bad H e 22
+--T1 ITAvGER r 30
+--T2 GBRvGER r 31
+--T3 ITAvGER r 32
+
+-- Canoe e 23 r 33
+
+-- VTT e 24 r 34
+
+-- judo F e 25
+--T1 KORvUKR r 35
+--T2 KORvBRA r 36
+--T3 BRAvUKR r 37
+
+-- beach e 26
+--t1 GREvRSA r 38
+--t2 GREvANG r 39
+--t3 RSAvANG r 40
+
+-- rugby e 27
+--t1 POLvGBR r41
+--t2 POLvBRA r42
+--t3 GBRvBRA r 43
+
+-- waterpolo e 28
+--t1 GBRvUSA r 44
+--t2 GBRvBRA r 45
+--t3 BRAvUSA r 46
+
+-- tennis e 29
+--1/2 1 NGRvKOR r 47
+--1/2 2 NEDvITA r 48
+--F KORvNED r 49
+--F 2 NGRvITA r 50
+
+-- lutte e 30
+--1/2 1 UKRvSUI r 51
+--1/2 2 FRAvITA r 52
+--F UKRvFRA r 53
+--F 2 ITAvSUI r 54
+
+
+--equitation e 31 r55
+
+-- escrime e 32 r56
+
+-- halter e 33 r57
+
+--plongeon e 34 r58
+
+-- tir e 35 r59
+
+-- arc e 36 r60
