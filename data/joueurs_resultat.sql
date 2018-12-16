596e8693e2529a5fa4b59739d6b115ca09e3ee24
dabauer <colinpseud@gmail.com>
2018-12-16 12:07:15 +0100

    debut entree resultat et joueurs_Resulat

---
+++ data/joueurs_resultat.sql
@@ -0,0 +1,73 @@
+INSERT INTO joueurs_resultat (joueurs, resultat) VALUES
+--Athletisme
+--F
+--100m e 4 r 1
+(1,1),
+(2,2),
+(3,3),
+--200m e 5 r 2
+(4,4),
+(5,5),
+(6,6),
+--400m e 6 r 3
+--M
+--100m e 1 r 4
+--200m e 2 r 5
+--400m e 3 r 6
+--Marathon e 8 r 7
+--110m haies e 7 r 8
+--3000m obstacles e 9 r 9
+--triple saut e 10 r 10
+
+--hand M e 12 
+--T1 ANGvPRK r 11
+--T2 ITAvPRK r 12
+--T3 ANGvITA r 13
+
+--hand F e 11
+--T1 JPNvUSA r 14
+--T2 FRAvFRA r 15
+--T3 FRAvUSA r 16
+
+--Natation
+
+--4x100nagelibre e 13 r 17
+--4x200nl e 15 r 18
+--4x1004n e 18 r 19
+
+--200mpap e 14 r 20
+--200m4n e 16 r 21
+--100mpap e 17 r 22
+
+--200m nl H e 20
+--1/2 r 23
+--F r 24
+
+--200m nl F e 19
+--1/2 r 25
+--F r 26
+
+--Football f e 21
+--T1 ITAvGBR r 27
+--T2 GERvGBR r 28
+--T3 ITAvGER r 29
+
+-- Bad H e 22
+--T1 ITAvGER r 30
+--T2 GBRvGER r 31
+--T3 ITAvGER r 32
+
+-- Canoe e 23 r 33
+
+-- VTT e 24 r 34
+
+-- judo F e 25
+--T1 KORvUKR r 35
+--T2 KORvBRA r 36
+--T3 BRAvUKR r 37
+
+-- beach e 26
+--t1 GREvRSA r 38
+--t2 GREvANG r 39
+--t3 RSAvANG r 40
+
